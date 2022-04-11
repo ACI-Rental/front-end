3c6a6859fee542b65d1683faf4dbd89ec35c5e06
Joris Kamminga <joris.kamminga@gmail.com>
2022-04-11 09:50:34 +0200

    added 2 pages and navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { HistoryPageComponent } from './pages/history-page/history-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomePageComponent,
+    HistoryPageComponent
   ],
   imports: [
     BrowserModule,
