1498b0ef4b89587c70f5bf8d6f9c3637a3ca42f3
Joris Kamminga <joris.kamminga@gmail.com>
2022-04-12 14:33:15 +0200

    Added http interceptrs and services

---
+++ src/app/interceptors/error-interceptor.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+
+    constructor(private accountService: AccountService) {}
+
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        //Logs out user if he isn't authorized
+        return next.handle(request).pipe(catchError(err => {
+            if (err.status === 401) {
+                // Get refresh token
+                this.accountService.Logout();
+            }
+            
+            const error = err.statusText;
+            return throwError(error);
+        }))
+    }
+}
+
+export const errorInterceptorProviders = [
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
+];
