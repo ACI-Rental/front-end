20af7fe89a56921fa53ee8295619c416212c3501
Bohlken,Joep J <joepbohlken@gmail.com>
2022-04-13 11:09:31 +0200

    finished table component

--- src/app/pages/history-page/history-page.component.ts
+++ src/app/pages/history-page/history-page.component.ts
@@ -9,8 +9,13 @@ export class HistoryPageComponent implements OnInit {
 
   constructor() { }
 
+  headers:Array<string> = ['From', 'To', 'Product', 'Status']
+  data:Array<any> = [
+  {from:'12-04-2002', to:'16-04-2002', product:'camera', status: 'renting'},
+  {from:'16-04-2002', to:'10-04-2002', product:'tripod', status: 'late'},
+  {from:'12-04-2002', to:'16-04-2002', product:'camera', status: 'returned'},
+  {from:'12-04-2002', to:'16-04-2002', product:'lamp', status: 'returned'}]
+
   ngOnInit(): void {
   }
-
-  
 }
