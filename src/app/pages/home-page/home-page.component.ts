43126b0d3fe2a82a70228937a5f8fe96d395513e
Joris Kamminga <joris.kamminga@gmail.com>
2022-04-18 20:42:19 +0200

    Added backdrop eventy listener

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
@@ -8,6 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class HomePageComponent implements OnInit {
 
   filterOpen: boolean = false;
+
+  @HostListener('document:click', ['$event'])
+  onClick(ev: any) {
+    const backdropRef: any = document.getElementsByClassName('product_filters_backdrop')[0];
+
+    if (ev.target === backdropRef) {
+      this.filterOpen = false;
+    }
+  }
 
   constructor() { }
 
